dd2513d7d600bffa9e450843300165cbfc56af85
albertassi88 <albertassi88@gmail.com>
2021-06-02 14:35:23 -0300

    Alteracoes em connection

--- models/mongodbConnection.js
+++ models/mongodbConnection.js
@@ -3,7 +3,18 @@ const { MongoClient } = require('mongodb');
 const URI = 'mongodb://localhost:27017/live_lecture_21_1';
 const DB_NAME = 'live_lecture_21_1';
 
-module.exports = async () => MongoClient.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then((connection) => connection.db(DB_NAME));
+let connection = null;
+
+module.exports = async function (collectionName) {
+    connection = connection ||
+    (await MongoClient.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }));
+
+    return connection.db(DB_NAME).collection(collectionName);
+};
+
+
+
+
