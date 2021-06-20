7080ceb58ce9d59c80ba5b28ab72a2ad9fe15191
albertassi88 <albertassi88@gmail.com>
2021-06-19 22:20:18 -0300

    exemplo deploy-heroku

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
-//DEPLOY - Deploy é publicar uma aplicação em um servidor e deixar ela disponível (pública).
+## DEPLOY - Deploy é publicar uma aplicação em um servidor e deixar ela disponível (pública).
 
-//Deploy com Heroku - (Heroku é aonde podemos armazenar e hospedar a nossa aplicação).
+## Deploy com Heroku - (Heroku é aonde podemos armazenar e hospedar a nossa aplicação).
 
-//Realizando o Deploy: 
+## Dyno - Exemplo: pega todos os arquivos de código e configuração que escrevemos e enfia em uma caixa e bota pra rodar, isso é um dyno.
+
+### Realizando o Deploy: 
 1 - heroku create // criar um novo app
 2 - heroku apps // lista os apps criados
 3 - heroku local web // rodar localmente o heroku
+4 - git add .
+5 - git commit -m "Comitando"
+6 - git push heroku master //enviando para o heroku
 
+heroku logs --app <nome da aplicação> exemplo: blomin-atol-59875 //acessar o log da aplicação
 
-"scripts": {
-    "start": "node index.js"
-},
+heroku logs --app -t <nome da aplicação> //monitora em tempo real, tipo o nodemon 
 
-Instalações:
+heroku config:set QUALQUER_NOME=homologacao --app <nome-de-aplicação> exemplo:blooming-atoll-66992 //criando chave de ambiente
+
+### Instalações:
 
 npm install
 npm i express
 npm init -y
+
+"scripts": {
+    "start": "node index.js"
+},
--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const express = require('express');
 
 const app = express();
 
+const QUALQUER_NOME = process.env.QUALQUER_NOME || 'Não foi:' //Variável de Ambiente
+/*heroku config:set QUALQUER_NOME=homologacao --app <nome-de-aplicação> exemplo:blooming-atoll-66992
+//Enviar a variável de ambiente que criei com o nome de homologação para o heroku*/
+
 app.get('/', (req, res) => {
-    res.send('Hello Trybe');
+    res.send(`Você está no ambiente de ${QUALQUER_NOME}`);
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Porta ${PORT}`));
+
+
+
+/*O que são variáveis de ambiente?
+
+Quando um programa é executado, ele recebe informações do ambiente em que ele está sendo executado. Essas informações de ambiente são 
+passadas implicitamente via variáveis de ambiente. Esses valores são, por exemplo, o locale do sistema, tipo do terminal, etc.
+Elas são valores nomeados dinamicamente no sistema operacional, que afeta o comportamentos dos programas que o consome. 
+Elas são guardadas em uma "lista" de chave-valor.
+Todos os valores das variáveis de ambiente são strings não nulas.
+Geralmente, guardamos configurações nessas variáveis, tanto do usuário logado no sistema, como system-wide.*/
+
+
+/*Quando devo usa-lás?
+
+Existem vários usos das variáveis de ambiente. Um uso frequente é para guardar chaves e senhas, embora existam métodos melhores. 
+Ao invés de você ter diversas constantes na sua aplicação para guardar chaves de API, senhas de banco de dados e essas coisas, 
+por que não colocá-las em variáveis de ambiente? */
